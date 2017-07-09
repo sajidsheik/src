3a509ad61dc5c2039c3b5b17e213b8e9123934c8
sajid sheik <ssajidsheik36@gmail.com>
2017-07-09 18:24:29 +0530

    Revert "1"
    This reverts commit de09db2006192ea59c2a14a57da0b76ab90738af.

--- components/navigation/leftNavbar/leftNavbar.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    'use strict';
-
-    angular.module('app.navigation.leftNavbar', [
-        'app.navigation.leftNavbar.directive'
-    ]);
-
-})();
--- shared/directives/notify/notify.module.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-
-    angular.module('app.service.notify', [
-        'app.service.notify.factory',
-        'app.service.notify.directive',
-        'app.service.notify.formError'
-    ])
-})();
